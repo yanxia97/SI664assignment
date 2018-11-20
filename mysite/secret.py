68cc1d36fcb15924cf1d6aba7d6877e0be6ed27d
yanxia97 <isaacyan@umich.edu>
2018-11-19 20:53:45 -0500

    Resubmit all the files

--- mysite/secret.py
+++
@@ -1,3 +0,0 @@
-SECRET_KEY = '3@o&br$jvsj4kbb)hj-v&68q$8(eon81clb2kw(em(0q)lz=pi'
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '1071401959543-05bc49gjts8ua9q6boq2lhjurhiootts.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'qvuL-htYyWrECZA7l9hZq9v2'
